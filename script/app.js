61d161a33088ee10dde9600690e23f0f5c13eb89
WillemsBrecht <brecht.willems2@student.howest.be>
2020-10-13 09:23:28 +0200

    Update app.js
    Added show text version of password (toggle password and text for input field)

--- script/app.js
+++ script/app.js
@@ -2,8 +2,24 @@ function handleFloatingLabel() {
    
 }
 
-function handlePasswordSwitcher() {
+const handlePasswordSwitcher = function () {
+    const passwordInput = document.querySelector('.js-password-input');
+    const passwordToggle = document.querySelector('.js-password-toggle');
     
+    // Simpele zelfcontrole
+    if(!passwordInput || !passwordToggle){
+        console.error('The classes were not loaded', passwordInput, passwordToggle);
+        return;
+    }
+    //We gaan luisteren of er geklikt wordt op de checkbox.
+    //Als er geklikt wordt, veranderen we het type van de input van 'password' naar 'text' en vice versa.
+    passwordToggle.addEventListener('change', function(){
+        if(passwordInput.type == 'password'){
+            passwordInput.type = 'text';
+        } else{
+            passwordInput.type = 'password';
+        }
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
